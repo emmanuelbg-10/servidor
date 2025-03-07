9255511b9a99081a3db633c83b3c5b62e9015a8f
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-03-07 03:24:58 +0000

    refactor: reorganizar la configuración del servidor y exportar la app para despliegue en Vercel

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
-const PORT = 3000;
+const app = express();
 
-// middlewares
+// Middlewares
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// routes
-app.use('/api/destinations',require('./routes/destinations'));
+// Rutas
+app.use('/api/destinations', require('./routes/destinations'));
 
-app.listen(3000, ()=>{
-    console.log("El servidor está escuchando en el puerto: "+PORT);
-});
+// Exportar la app para que Vercel la maneje correctamente
+module.exports = app;
